e433fae5db953765525986f14bedcfdbf058802e
Acharekar, Chetan <chetan.acharekar@here.com>
2021-04-27 15:35:26 +0530

    added donate section

--- pages/contribute.js
+++ pages/contribute.js
@@ -39,9 +39,19 @@ export default function Contribute() {
                         You can help
                     </Typography>
                     <Typography className={classes.pos} color="textSecondary">
-
+                        <br/>
+                        We know you may like to support covid relief effort. So we are trying to help you with details of relevant, impacted NGO. You can choose donate/volunteer on below given websites. We don't have any control or  interest with any parties. You can verify and select wisely. We do appreciate your feedback/observation.
+                    </Typography>
+                    <Typography >
+                        <ul>
+                            <a href="https://www.donatekart.com/Hemkunt-Foundation/Give-Oxygen-Cylinders/"><li>https://www.donatekart.com/Hemkunt-Foundation/Give-Oxygen-Cylinders/</li></a>
+                            <a href="https://hemkuntfoundation.com/donate-now/"><li>https://hemkuntfoundation.com/donate-now/</li></a>
+                            <a href="https://hemkuntfoundation.com/volunteer/"><li>Volunteer with NGO</li></a>
+                        </ul>
                     </Typography>
                     <Typography variant="body2" component="p">
+                        <br/>
+                        <br/>
                         If you have any information which you think is useful and world should know about it,
                         Please share with us. We try and publish only authentic sources.However it is really difficult job.
                         Hence please dont spam and waste everyone's precious time.
